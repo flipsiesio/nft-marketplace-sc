2e2b18d793b6a05cea0623e5b2eece8b8cd62696
IntegralTeam <repo@sfxdx.ru>
2022-01-12 13:41:21 +0700

    testnet deploy update

--- tronbox-poker-marketplace-sc/migrations/2_deploy_contracts.js
+++ tronbox-poker-marketplace-sc/migrations/2_deploy_contracts.js
@@ -1,48 +1,15 @@
 const MockNFT = artifacts.require("./mock/MockNFT.sol");
 const NFTMarketplace = artifacts.require("./NFTMarketplace.sol");
-
 const Card = artifacts.require("./Card.sol");
 const CardFactory = artifacts.require("./CardFactory.sol");
 const CardRandomMinter = artifacts.require("./CardRandomMinter.sol");
-
 const { time } = require("@openzeppelin/test-helpers");
 
-module.exports = function (deployer, network, accounts) {
-  const feeInBps = 9500;
-  deployer.then(() => {
-    deployer.deploy(Card).then(nftInstance => {
-      deployer.deploy(
-        NFTMarketplace,
-        nftInstance.address,
-        accounts[0],
-        time.duration.days(1),
-        time.duration.days(10),
-        feeInBps
-      ).then(nftMarketplaceInstance => {
-        deployer.deploy(CardFactory, nftInstance.address).then(nftFactoryInstance => {
-          deployer.deploy(CardRandomMinter, nftFactoryInstance.address).then(randomMinter => {
-            nftFactoryInstance.setOptionMinter(randomMinter.address, true, { from: accounts[0] })
-              .then(setOptionMinterReceipt => {
-                console.log(`Set option minter: ${setOptionMinterReceipt}`);
-                nftInstance.setMinterRole(nftFactoryInstance.address, true, { from: accounts[0] })
-                  .then(setMinterRoleReceipt => {
-                    console.log(`Set minter role at NFT: ${setMinterRoleReceipt}`);
-                    console.log("=== Addresses and params ===");
-                    console.log(
-                      nftInstance.address,
-                      nftMarketplaceInstance.address,
-                      nftFactoryInstance.address,
-                      randomMinter.address,
-                      accounts[0],
-                      time.duration.days(1),
-                      time.duration.days(10),
-                      feeInBps
-                    );
-                  });
-              });
-          });
-        });
-      });
-    });
-  });
-};
+module.exports = async function (deployer, network, accounts) {
+    const feeInBps = 9500;
+    const account = "TLHVUe1sizxRUctjpsKj1iRWkhUkdRs8KW";
+    await deployer.deploy(Card);
+    await deployer.deploy(NFTMarketplace, Card.address, account, time.duration.days(1).toNumber(), time.duration.days(10).toNumber(), feeInBps);
+    await deployer.deploy(CardFactory, Card.address);
+    await deployer.deploy(CardRandomMinter, CardFactory.address);
+}
