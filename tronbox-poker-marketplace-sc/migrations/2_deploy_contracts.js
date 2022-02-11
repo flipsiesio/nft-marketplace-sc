fcaeaa2f1acf2ccc8ff4cf4129ed6d2afe01745d
IntegralTeam <repo@sfxdx.com>
2022-02-11 19:13:31 +0700

    Update deploy script

--- tronbox-poker-marketplace-sc/migrations/2_deploy_contracts.js
+++ tronbox-poker-marketplace-sc/migrations/2_deploy_contracts.js
@@ -1,6 +1,4 @@
-const MockNFT = artifacts.require("./mock/MockNFT.sol");
 const NFTMarketplace = artifacts.require("./NFTMarketplace.sol");
-const NFTAuction = artifacts.require("./NFTAuction.sol");
 const NFTSale = artifacts.require("./NFTSale.sol");
 const Card = artifacts.require("./Card.sol");
 const CardFactory = artifacts.require("./CardFactory.sol");
@@ -12,8 +10,19 @@ module.exports = async function (deployer, network, accounts) {
     const account = "TLHVUe1sizxRUctjpsKj1iRWkhUkdRs8KW";
     await deployer.deploy(Card);
     await deployer.deploy(NFTMarketplace, Card.address, account, time.duration.days(1).toNumber(), time.duration.days(10).toNumber(), feeInBps);
-    await deployer.deploy(NFTAuction, Card.address, account, time.duration.days(1).toNumber(), time.duration.days(10).toNumber(), feeInBps);
     await deployer.deploy(NFTSale, Card.address, account, time.duration.days(1).toNumber(), time.duration.days(10).toNumber(), feeInBps);
     await deployer.deploy(CardFactory, Card.address);
     await deployer.deploy(CardRandomMinter, CardFactory.address);
+
+    let card = await Card.deployed();
+    let factory = await CardFactory.deployed();
+
+    await card.setMinterRole(CardFactory.address, true);
+    await factory.setMinterRole(CardRandomMinter.address, true);
+
+    await factory.setIdBoundaryForOption(0, 0, 6500);
+    await factory.setIdBoundaryForOption(1, 6500, 7271);
+    await factory.setIdBoundaryForOption(2, 7271, 7771);
+    await factory.setIdBoundaryForOption(3, 7771, 7796);
+    await factory.setIdBoundaryForOption(4, 7796, 7803);
 }
