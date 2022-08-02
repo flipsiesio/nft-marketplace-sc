e8b0b686d022891c011a556bdf366dcea1563da8
IntegralTeam <repo@sfxdx.com>
2022-08-02 23:14:46 +0300

    Delete Unused Interface

--- contracts/interfaces/IMintable.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-interface IMintable {
-    function mint(address _to, uint256 _tokenId) external;
-
-    function exists(uint256 _tokenId) external view returns (bool);
-}
